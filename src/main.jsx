cae81046d0054b9cf34bdccc68eaf0c4acb4c9fa
yenommik-maker <yenommik@gmail.com>
2026-07-06 21:51:26 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import SmartCampusPrototype from "./App.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <SmartCampusPrototype />
+  </React.StrictMode>,
+);
